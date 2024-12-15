1dd95fa0e0dc484fc6ea8477c1cca1b6e2bc0225
seoungwon <gavrakr@naver.com>
2024-12-16 00:23:38 +0900

    2024-12-16-00:23

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const timeNow = document.querySelector("#clock .clock__time");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  timeNow.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "7a4d14d9f2fd49d16e07ad4100df3d33";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = ` ${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Cant'find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
