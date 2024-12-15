b0b8fb42c0efae845765196c3e685e68da12e0cb
seoungwon <gavrakr@naver.com>
2024-12-16 01:03:25 +0900

    2024-12-16-01:03

--- js/background.js
+++ js/background.js
@@ -1,8 +1,7 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
-
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgimg = document.createAttribute("background");
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-
-document.body.append(bgImage);
+bgImage = `img/${chosenImage}`;
+bg = document.querySelector("#bg");
+bg.background = bgImage;
--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,7 @@ function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  timeNow.innerText = `${hours}:${minutes}:${seconds}`;
+  timeNow.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
